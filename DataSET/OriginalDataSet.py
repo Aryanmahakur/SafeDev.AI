d483f314cf106ccf4f7b7355d7e037debfabed50
Aryanmahakur <mahakuraryan@gmail.com>
2026-09-15 23:56:31 +0530

    uf

--- DataSET/OriginalDataSet.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def loadDataset():
-    a = pd.read_csv(r"c:\Users\ARYAN MAHAKUR\Downloads\archive (6)\cicids2017_cleaned.csv")
-    return a
-
-loadDataset()

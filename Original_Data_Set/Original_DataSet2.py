3cd24489b7aefc8dc83ee37c5da3c6074cbb44ae
Aryanmahakur <mahakuraryan@gmail.com>
2026-09-16 14:55:16 +0530

    Original Data Set

--- Original_Data_Set/Original_DataSet2.py
+++ Original_Data_Set/Original_DataSet2.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 def loaddata2():
     df=pd.read_csv(r"C:\Users\ARYAN MAHAKUR\Desktop\DataSet\MachineLearningCVE\Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv")
+    
     return df
 
